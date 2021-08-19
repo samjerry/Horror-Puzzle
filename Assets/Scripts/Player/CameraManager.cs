d3f5f215ea29d12fcbb0b98d1b44a916f2ba70c5
MarrowOfAlbion <jerry.sam@ziggo.nl>
2021-08-20 01:54:10 +0200

    Added a SetTransform function to CameraManager.cs, Moved Start() functionality into SetTransfrom, Added a Rotation and Position check in Update() triggers SetTransform()

--- Assets/Scripts/Player/CameraManager.cs
+++ Assets/Scripts/Player/CameraManager.cs
@@ -7,7 +7,28 @@ public class CameraManager : MonoBehaviour
     [SerializeField] GameObject _target;
     [SerializeField] float _yOffset;
 
+    Quaternion _oldRot;
+    Vector3 _oldPos;
+
     void Start()
+    {
+        SetTransform();
+    }
+
+    private void Update()
+    {
+        if (transform.position != _oldPos)
+        {
+            SetTransform();
+        }
+
+        if (transform.rotation != _oldRot)
+        {
+            SetTransform();
+        }
+    }
+
+    void SetTransform()
     {
         transform.position = new Vector3(_target.transform.position.x, _target.transform.position.y + _yOffset, _target.transform.position.z);
         transform.rotation = _target.transform.rotation;
