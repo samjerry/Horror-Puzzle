cb6b7bf5ecaa8d8e45b6ee2a022b725ad90440ac
MarrowOfAlbion <jerry.sam@ziggo.nl>
2021-08-20 01:46:44 +0200

    Changed variable name: _heightOffset -> _yOffset

--- Assets/Scripts/Player/CameraManager.cs
+++ Assets/Scripts/Player/CameraManager.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class CameraManager : MonoBehaviour
 {
     [SerializeField] GameObject _target;
-    [SerializeField] float _heightOffset;
+    [SerializeField] float _yOffset;
 
     void Start()
     {
-        transform.position = new Vector3(_target.transform.position.x, _target.transform.position.y + _heightOffset, _target.transform.position.z);
+        transform.position = new Vector3(_target.transform.position.x, _target.transform.position.y + _yOffset, _target.transform.position.z);
         transform.rotation = _target.transform.rotation;
     }
 }
