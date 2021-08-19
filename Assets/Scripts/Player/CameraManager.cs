9b7a6555b46971df354d8564b6e24c3c372e4825
MarrowOfAlbion <jerry.sam@ziggo.nl>
2021-08-20 01:45:29 +0200

    Added repositioning and rotation to the Start function in CameraManager.cs

--- Assets/Scripts/Player/CameraManager.cs
+++ Assets/Scripts/Player/CameraManager.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject _target;
+    [SerializeField] float _heightOffset;
+
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        transform.position = new Vector3(_target.transform.position.x, _target.transform.position.y + _heightOffset, _target.transform.position.z);
+        transform.rotation = _target.transform.rotation;
     }
 }
