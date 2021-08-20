567e3d695966078ab3ded992c831934bc4b3aa82
MarrowOfAlbion <jerry.sam@ziggo.nl>
2021-08-20 03:37:06 +0200

    Added Keybindings to PlayerMovement.cs, Changed run movement type to sprint, Added a walk movement type, Removed _pos variable, Added multi-button pressed down check in PlayerMovement.cs

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,61 +2,113 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-enum MovementTypes { walk, run, crouch};
+enum MovementTypes { walk, run, sprint, crouch};
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] KeyCode _forwardKey;
+    [SerializeField] KeyCode _backwardKey;
+    [SerializeField] KeyCode _leftKey;
+    [SerializeField] KeyCode _rightKey;
+    [SerializeField] KeyCode _crouchKey;
+    [SerializeField] KeyCode _walkKey;
+    [SerializeField] KeyCode _sprintKey;
+
     [SerializeField] MovementTypes _pMovement;
 
-    [SerializeField] float _walkSpd = 1;
-    [SerializeField] float _crouchSpd = 0.5f;
-    [SerializeField] float _runSpd = 1.5f;
+    [SerializeField] float _runSpd = 1f;
+    [SerializeField] float _walkSpd = 0.5f;
+    [SerializeField] float _crouchSpd = 0.25f;
+    [SerializeField] float _sprintSpd = 1.5f;
     [SerializeField] float _spd;
 
     bool _isGrounded;
 
-    Vector3 _pos;
-
     void Start()
     {
-        _pos = transform.position;
-        _pMovement = MovementTypes.walk;
+        _pMovement = MovementTypes.run;
 
-        SetSpeed(_pMovement.ToString());
+        SetSpeed();
     }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
+
+        if (Input.GetKey(_sprintKey))
         {
-            _pos += Vector3.right * _spd * Time.deltaTime;
+            _pMovement = MovementTypes.sprint;
+            SetSpeed();
         }
-        if (Input.GetKey(KeyCode.A))
+        
+        if (Input.GetKey(_walkKey))
         {
-            _pos += Vector3.left * _spd * Time.deltaTime;
+            _pMovement = MovementTypes.walk;
+            SetSpeed();
         }
-        if (Input.GetKey(KeyCode.W))
+
+        if (Input.GetKey(_crouchKey))
         {
-            _pos += Vector3.forward * _spd * Time.deltaTime;
+            _pMovement = MovementTypes.crouch;
+            SetSpeed();
+
+            if (Input.GetKey(_sprintKey))
+            {
+                _spd = _crouchSpd * 1.5f;
+            }
         }
-        if (Input.GetKey(KeyCode.S))
+
+        if (Input.GetKeyUp(_crouchKey) && !Input.GetKey(_sprintKey) && !Input.GetKey(_walkKey))
         {
-            _pos += Vector3.back * _spd * Time.deltaTime;
+            _pMovement = MovementTypes.run;
+            SetSpeed();
+        }
+        
+        if (Input.GetKeyUp(_walkKey) && !Input.GetKey(_crouchKey) && !Input.GetKey(_sprintKey))
+        {
+            _pMovement = MovementTypes.run;
+            SetSpeed();
+        }
+
+        if (Input.GetKeyUp(_sprintKey) && !Input.GetKey(_crouchKey) && !Input.GetKey(_walkKey))
+        {
+            _pMovement = MovementTypes.run;
+            SetSpeed();
+        } 
+        
+
+        if (Input.GetKey(_rightKey))
+        {
+            transform.position += (Vector3.right * _spd * Time.deltaTime);
+        }
+        if (Input.GetKey(_leftKey))
+        {
+            transform.position += (Vector3.left * _spd * Time.deltaTime);
+        }
+        if (Input.GetKey(_forwardKey))
+        {
+            transform.position += (Vector3.forward * _spd * Time.deltaTime);
+        }
+        if (Input.GetKey(_backwardKey))
+        {
+            transform.position += (Vector3.back * _spd * Time.deltaTime);
         }
     }
 
-    void SetSpeed(string _moveType)
+    void SetSpeed()
     {
-        switch (_moveType)
+        switch (_pMovement.ToString())
         {
-            case "walk":
-                _spd = _walkSpd;
-                break;
             case "run":
                 _spd = _runSpd;
                 break;
+            case "sprint":
+                _spd = _sprintSpd;
+                break;
             case "crouch":
                 _spd = _crouchSpd;
+                break;
+            case "walk":
+                _spd = _walkSpd;
                 break;
 
             default:
