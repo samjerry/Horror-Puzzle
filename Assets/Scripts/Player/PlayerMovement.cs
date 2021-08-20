260a23075582be52beec0c8927506c0b367addbc
MarrowOfAlbion <jerry.sam@ziggo.nl>
2021-08-20 02:26:26 +0200

    Added Movement types enum, Added movement type speeds

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,16 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum MovementTypes { walk, run, crouch};
+
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] MovementTypes _pMovement;
+
+    [SerializeField] float _walkSpd = 1;
+    [SerializeField] float _crouchSpd = 0.5f;
+    [SerializeField] float _runSpd = 1.5f;
 
     void Start()
     {
-        
+        _pMovement = MovementTypes.walk;
     }
 
     void Update()
     {
-        
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            var a = transform.TransformDirection(0, 0, 50); //up
+            controller.SimpleMove(a * speed);
+        }
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            var b = transform.TransformDirection(0, 50, 0); // down
+            controller.SimpleMove(b * speed);
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            var c = transform.TransformDirection(50, 0, 0); //right
+            controller.SimpleMove(c * speed);
+        }
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            var d = transform.TransformDirection(-50, 0, 0); // left
+            controller.SimpleMove(d * speed);
+        }
     }
 }
