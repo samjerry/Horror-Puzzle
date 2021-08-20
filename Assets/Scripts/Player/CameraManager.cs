708aea0b9caa1af90c4081f817318f48cc3678d2
MarrowOfAlbion <jerry.sam@ziggo.nl>
2021-08-20 16:27:49 +0200

    Changed the player movement script and camera manager

--- Assets/Scripts/Player/CameraManager.cs
+++ Assets/Scripts/Player/CameraManager.cs
@@ -2,168 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
 public class CameraManager : MonoBehaviour
 {
     [SerializeField] GameObject _target;
     [SerializeField] float _yOffset;
 
-    Quaternion _oldRot;
-    Vector3 _oldPos;
+    [SerializeField] float _sensitivityX = 100F;
+    [SerializeField] float _sensitivityY = 100F;
 
-    [SerializeField] RotationAxes _axes = RotationAxes.MouseXAndY;
+    [SerializeField] float _minimumX = -90F;
+    [SerializeField] float _maximumX = 90F;
+
+    [SerializeField] float _xRot;
+    [SerializeField] float _yRot;
 
 
-    [SerializeField] float _sensitivityX = 15F;
-    [SerializeField] float _sensitivityY = 15F;
-
-    [SerializeField] float _minimumX = -360F;
-    [SerializeField] float _maximumX = 360F;
-
-    [SerializeField] float _minimumY = -60F;
-    [SerializeField] float _maximumY = 60F;
-
-    [SerializeField] float _rotationX = 0F;
-    [SerializeField] float _rotationY = 0F;
-
-    private List<float> _rotArrayX = new List<float>();
-    float _rotAverageX = 0F;
-
-    private List<float> _rotArrayY = new List<float>();
-    float _rotAverageY = 0F;
-
-    public float _frameCounter = 20;
-
-    Quaternion _originalRotation;
-
-    void Start()
+    private void Start()
     {
-        Rigidbody rb = _target.GetComponent<Rigidbody>();
-        if (rb) { rb.freezeRotation = true; }
-
-        _originalRotation = transform.localRotation;
-        SetTransform();
+        Cursor.lockState = CursorLockMode.Locked;    
     }
 
     private void Update()
     {
-        if (transform.position != _oldPos) { SetTransform(); }
-        if (transform.rotation != _oldRot) { SetTransform(); }
-
         /// <summary>
         ///  Mouse Input
         /// <summary>
+        /// 
+        float _mouseX = Input.GetAxis("Mouse X") * _sensitivityX * Time.deltaTime;
+        float _mouseY = Input.GetAxis("Mouse Y") * _sensitivityY * Time.deltaTime;
 
-        if (_axes == RotationAxes.MouseXAndY)
-        {
-            _rotAverageY = 0f;
-            _rotAverageX = 0f;
+        _xRot -= _mouseY;
+        _xRot = Mathf.Clamp(_xRot, _minimumX, _maximumX);
 
-            _rotationY += Input.GetAxis("Mouse Y") * _sensitivityY;
-            _rotationX += Input.GetAxis("Mouse X") * _sensitivityX;
-
-            _rotArrayY.Add(_rotationY);
-            _rotArrayX.Add(_rotationX);
-
-            if (_rotArrayY.Count >= _frameCounter)
-            {
-                _rotArrayY.RemoveAt(0);
-            }
-            if (_rotArrayX.Count >= _frameCounter)
-            {
-                _rotArrayX.RemoveAt(0);
-            }
-
-            for (int j = 0; j < _rotArrayY.Count; j++)
-            {
-                _rotAverageY += _rotArrayY[j];
-            }
-            for (int i = 0; i < _rotArrayX.Count; i++)
-            {
-                _rotAverageX += _rotArrayX[i];
-            }
-
-            _rotAverageY /= _rotArrayY.Count;
-            _rotAverageX /= _rotArrayX.Count;
-
-            _rotAverageY = ClampAngle(_rotAverageY, _minimumY, _maximumY);
-            _rotAverageX = ClampAngle(_rotAverageX, _minimumX, _maximumX);
-
-            Quaternion yQuaternion = Quaternion.AngleAxis(_rotAverageY, Vector3.left);
-            Quaternion xQuaternion = Quaternion.AngleAxis(_rotAverageX, Vector3.up);
-
-            transform.localRotation = _originalRotation * xQuaternion * yQuaternion;
-        }
-        else if (_axes == RotationAxes.MouseX)
-        {
-            _rotAverageX = 0f;
-
-            _rotationX += Input.GetAxis("Mouse X") * _sensitivityX;
-
-            _rotArrayX.Add(_rotationX);
-
-            if (_rotArrayX.Count >= _frameCounter)
-            {
-                _rotArrayX.RemoveAt(0);
-            }
-            for (int i = 0; i < _rotArrayX.Count; i++)
-            {
-                _rotAverageX += _rotArrayX[i];
-            }
-            _rotAverageX /= _rotArrayX.Count;
-
-            _rotAverageX = ClampAngle(_rotAverageX, _minimumX, _maximumX);
-
-            Quaternion xQuaternion = Quaternion.AngleAxis(_rotAverageX, Vector3.up);
-            transform.localRotation = _originalRotation * xQuaternion;
-        }
-        else
-        {
-            _rotAverageY = 0f;
-
-            _rotationY += Input.GetAxis("Mouse Y") * _sensitivityY;
-
-            _rotArrayY.Add(_rotationY);
-
-            if (_rotArrayY.Count >= _frameCounter)
-            {
-                _rotArrayY.RemoveAt(0);
-            }
-            for (int j = 0; j < _rotArrayY.Count; j++)
-            {
-                _rotAverageY += _rotArrayY[j];
-            }
-            _rotAverageY /= _rotArrayY.Count;
-
-            _rotAverageY = ClampAngle(_rotAverageY, _minimumY, _maximumY);
-
-            Quaternion yQuaternion = Quaternion.AngleAxis(_rotAverageY, Vector3.left);
-            transform.localRotation = _originalRotation * yQuaternion;
-        }
+        transform.localRotation = Quaternion.Euler(_xRot, 0f, 0f);
+        _target.transform.Rotate(Vector3.up * _mouseX);
 
     }
-
-    void SetTransform()
-    {
-        transform.position = new Vector3(_target.transform.position.x, _target.transform.position.y + _yOffset, _target.transform.position.z);
-        transform.rotation = _target.transform.rotation;
-    }
-
-    private static float ClampAngle(float angle, float min, float max)
-    {
-        angle = angle % 360;
-        if ((angle >= -360F) && (angle <= 360F))
-        {
-            if (angle < -360F)
-            {
-                angle += 360F;
-            }
-            if (angle > 360F)
-            {
-                angle -= 360F;
-            }
-        }
-        return Mathf.Clamp(angle, min, max);
-    }
-
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,24 +5,35 @@ using UnityEngine;
 enum MovementTypes { walk, run, sprint, crouch};
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] KeyCode _forwardKey;
-    [SerializeField] KeyCode _backwardKey;
-    [SerializeField] KeyCode _leftKey;
-    [SerializeField] KeyCode _rightKey;
-    [SerializeField] KeyCode _crouchKey;
-    [SerializeField] KeyCode _walkKey;
-    [SerializeField] KeyCode _sprintKey;
+    [SerializeField] private KeyCode _forwardKey;
+    [SerializeField] private KeyCode _backwardKey;
+    [SerializeField] private KeyCode _leftKey;
+    [SerializeField] private KeyCode _rightKey;
+    [SerializeField] private KeyCode _crouchKey;
+    [SerializeField] private KeyCode _walkKey;
+    [SerializeField] private KeyCode _sprintKey;
 
-    [SerializeField] MovementTypes _pMovement;
-    
+    [SerializeField] private MovementTypes _pMovement;
 
-    [SerializeField] float _runSpd = 1f;
-    [SerializeField] float _walkSpd = 0.5f;
-    [SerializeField] float _crouchSpd = 0.25f;
-    [SerializeField] float _sprintSpd = 1.5f;
-    [SerializeField] float _spd;
+    [SerializeField] private CharacterController _controller;
 
-    bool _isGrounded;
+    [SerializeField] private float _runSpd = 1f;
+    [SerializeField] private float _walkSpd = 0.5f;
+    [SerializeField] private float _crouchSpd = 0.25f;
+    [SerializeField] private float _sprintSpd = 1.5f;
+    [SerializeField] private float _spd;
+    [SerializeField] private float _gravity;
+
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private float _checkDistance = 0.4f;
+    [SerializeField] private LayerMask groundMask;
+
+    private bool _isGrounded;
+
+    private Vector3 _velocity;
+
+    private float _x;
+    private float _z;
 
     void Start()
     {
@@ -34,6 +45,12 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        _isGrounded = Physics.CheckSphere(_groundCheck.position, _checkDistance, groundMask);
+
+        if (_isGrounded && _velocity.y < 0)
+        {
+            _velocity.y = -2f;
+        }
 
         /// <summary>
         /// Keyboard Input.
@@ -53,11 +70,6 @@ public class PlayerMovement : MonoBehaviour
         {
             _pMovement = MovementTypes.crouch;
             SetSpeed();
-
-            if (Input.GetKey(_sprintKey))
-            {
-                _spd = _crouchSpd * 1.5f;
-            }
         }
         if (Input.GetKeyUp(_crouchKey) && !Input.GetKey(_sprintKey) && !Input.GetKey(_walkKey))
         {
@@ -73,27 +85,18 @@ public class PlayerMovement : MonoBehaviour
         {
             _pMovement = MovementTypes.run;
             SetSpeed();
-        } 
-        
-        if (Input.GetKey(_rightKey))
-        {
-            transform.position += (Vector3.right * _spd * Time.deltaTime);
-        }
-        if (Input.GetKey(_leftKey))
-        {
-            transform.position += (Vector3.left * _spd * Time.deltaTime);
-        }
-        if (Input.GetKey(_forwardKey))
-        {
-            transform.position += (Vector3.forward * _spd * Time.deltaTime);
-        }
-        if (Input.GetKey(_backwardKey))
-        {
-            transform.position += (Vector3.back * _spd * Time.deltaTime);
         }
 
-        transform.rotation = GameObject.Find("Main Camera").transform.rotation;
+        _x = Input.GetAxis("Horizontal");
+        _z = Input.GetAxis("Vertical");
 
+        Vector3 _move = transform.right * _x + transform.forward * _z;
+
+        _controller.Move(_move * _spd * Time.deltaTime);
+
+        _velocity.y += _gravity * Time.deltaTime;
+
+        _controller.Move(_velocity * Time.deltaTime);
     }
 
     void SetSpeed()
