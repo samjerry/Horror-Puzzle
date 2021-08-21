36a578a3471d9f64e044c14630d37d2a865f41c2
MarrowOfAlbion <jerry.sam@ziggo.nl>
2021-08-21 17:15:19 +0200

    Added Event.cs, Added amountgoal var to EventTrigger.cs

--- Assets/Scripts/Interaction/EventTrigger.cs
+++ Assets/Scripts/Interaction/EventTrigger.cs
@@ -8,12 +8,20 @@ public class EventTrigger : MonoBehaviour
     [SerializeField] TriggerTypes _trigger;
 
     [SerializeField] private int _amount = 0;
+    [SerializeField] private int _amountGoal = 0;
 
-    [SerializeField] private float _range;
+    [SerializeField] private float _range = 2.5f;
 
     [SerializeField] private bool _isTriggered = false;
 
     [SerializeField] List<GameObject> _triggerObjects;
-    
 
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        
+    }
 }
