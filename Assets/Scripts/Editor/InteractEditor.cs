7775e241c1aa5db2679cff563f1c57f2410fb311
MarrowOfAlbion <jerry.sam@ziggo.nl>
2021-08-24 23:45:08 +0200

    Removed istriggeredprop from main OnInspectorGUI, Added istriggeredprop to triggertype area and count

--- Assets/Scripts/Editor/InteractEditor.cs
+++ Assets/Scripts/Editor/InteractEditor.cs
@@ -32,12 +32,7 @@ public class InteractEditor : Editor
 
     public override void OnInspectorGUI()
     {
-        isTriggeredProp = serializedObject.FindProperty("isTriggered");
-        EditorGUILayout.PropertyField(isTriggeredProp, new GUIContent("Event Triggered"));
-        _iManager.isTriggered = isTriggeredProp.boolValue;
-
         _iManager.tType = (InteractManager.TriggerType)EditorGUILayout.EnumPopup("Trigger Type", _iManager.tType);
-       
         
         switch (_iManager.tType)
         {
@@ -48,18 +43,21 @@ public class InteractEditor : Editor
 
             case InteractManager.TriggerType.Count:
                 {
+                    isTriggeredProp = serializedObject.FindProperty("isTriggered");
                     amountProp = serializedObject.FindProperty("amount");
 
                     EditorGUILayout.PropertyField(amountProp, new GUIContent("Trigger Amount"));
+                    EditorGUILayout.PropertyField(isTriggeredProp, new GUIContent("Event Triggered"));
 
                     _iManager.amount = amountProp.intValue;
-
+                    _iManager.isTriggered = isTriggeredProp.boolValue;
 
                     break;
                 }
 
             case InteractManager.TriggerType.Area:
                 {
+                    isTriggeredProp = serializedObject.FindProperty("isTriggered");
                     areaWidthProp = serializedObject.FindProperty("areaWidth");
                     areaHeightProp = serializedObject.FindProperty("areaHeight");
                     areaDepthProp = serializedObject.FindProperty("areaDepth");
@@ -67,10 +65,13 @@ public class InteractEditor : Editor
                     EditorGUILayout.PropertyField(areaWidthProp, new GUIContent("Trigger Width"));
                     EditorGUILayout.PropertyField(areaHeightProp, new GUIContent("Trigger Height"));
                     EditorGUILayout.PropertyField(areaDepthProp, new GUIContent("Trigger Depth"));
+                    EditorGUILayout.PropertyField(isTriggeredProp, new GUIContent("Event Triggered"));
+
 
                     _iManager.areaWidth = areaWidthProp.floatValue;
                     _iManager.areaHeight = areaHeightProp.floatValue;
                     _iManager.areaDepth = areaDepthProp.floatValue;
+                    _iManager.isTriggered = isTriggeredProp.boolValue;
 
                     break;
                 }
